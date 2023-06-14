fff1016efd638dc02fd0b499a76dfb721775772e
Priscilla Baah <pyfbaah@gmail.com>
2023-06-14 17:04:27 +0000

    create shorten url response serializer

--- api/url/serializers.py
+++ api/url/serializers.py
@@ -5,4 +5,9 @@ shorten_url_serializer = {
         'target_url': fields.String(required=True, description='url you want to shorten')              
 }
 
+shorten_url_response_serializer = {
+        'short_url': fields.String(required=True, description='short version of inputted url'),
+        'target_url': fields.String(required=True, description='actual url short url redirects to')              
+}
+
 

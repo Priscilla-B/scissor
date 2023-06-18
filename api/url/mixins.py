c5e043e2d46cc88cb0b6f5c2f198dd800b231347
Priscilla Baah <pyfbaah@gmail.com>
2023-06-18 22:03:02 +0000

    add url generation code

--- api/url/mixins.py
+++ api/url/mixins.py
@@ -4,6 +4,7 @@ import random
 
 class UrlHelpersMixin(object):
 
-    def generate_short_url(self, n):
-        short_url = ''.join(random.choices(string.ascii_letters, k=n))
+    def generate_short_url(self, n, request):
+        random_code = ''.join(random.choices(string.ascii_letters, k=n))
+        short_url = request.url_root + random_code
         return short_url

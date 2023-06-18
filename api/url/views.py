4715b9858b586440fc02cf96627e83475515a065
Priscilla Baah <pyfbaah@gmail.com>
2023-06-18 22:03:29 +0000

    add associated user to url shortening event

--- api/url/views.py
+++ api/url/views.py
@@ -3,6 +3,8 @@ from flask_jwt_extended import (jwt_required, get_jwt_identity)
 from flask_restx import Namespace, Resource, marshal
 from http import HTTPStatus
 
+from api.auth.models import User
+
 from .models import Url
 from .serializers import shorten_url_serializer, shorten_url_response_serializer
 from .mixins import UrlHelpersMixin
@@ -18,7 +20,7 @@ shorten_url_response_model = url_namespace.model(
 )
 
 @url_namespace.route('/shorten_url')
-class CreateUser(Resource, UrlHelpersMixin):
+class ShortenUrl(Resource, UrlHelpersMixin):
 
     @url_namespace.doc(description="Shorten a given url")
     @url_namespace.expect(shorten_url_model)
@@ -29,9 +31,12 @@ class CreateUser(Resource, UrlHelpersMixin):
         """
         data = request.get_json()
 
+        user_id = get_jwt_identity()
         new_url = Url(
-            target_url = data.target_url,
-            short_url = self.generate_short_url(5)
+            target_url = data["target_url"],
+            short_url = self.generate_short_url(5, request),
+            user_id = user_id,
+            user = User.get_by_id(user_id)
 
         )
 

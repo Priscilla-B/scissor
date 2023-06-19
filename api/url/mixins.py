a05167546fa4a56afc3fef6b09084c6d314a7c15
Priscilla Baah <pyfbaah@gmail.com>
2023-06-19 17:06:25 +0000

    update url generation logic

--- api/url/mixins.py
+++ api/url/mixins.py
@@ -4,7 +4,20 @@ import random
 
 class UrlHelpersMixin(object):
 
-    def generate_short_url(self, n, request):
-        random_code = ''.join(random.choices(string.ascii_letters, k=n))
-        short_url = request.url_root + random_code
-        return short_url
+    def generate_short_text(self, request, n):
+
+        short_text = request.get_json().get('custom_short_text')
+
+        if not short_text:
+            short_text = ''.join(random.choices(string.ascii_letters, k=n))
+        return short_text
+    
+    def get_url_domain(self, request):
+        domain = request.get_json().get('custom_domain')
+        if not domain:
+            domain = request.url_root
+
+        return domain
+    
+      
+        

6d604da92b95837036c0585564ac9454839e2856
Priscilla Baah <pyfbaah@gmail.com>
2023-06-19 17:05:29 +0000

    refactor serializer logic

--- api/auth/serializers.py
+++ api/auth/serializers.py
@@ -1,11 +1,18 @@
 from flask_restx import fields
 
 
-create_user_serializer = {
+base_user_serializer = {
         'first_name': fields.String(required=True, description='first name of user'),
         'last_name': fields.String(required=True, description='last name of user'),
         'username': fields.String(required=True, description='public display name of user'),
-        'email': fields.String(required=True, description='user email address'),
+        'email': fields.String(required=True, description='user email address')     
+        }
+
+get_user_serializer = {
+        'id': fields.Integer(),
+        } | base_user_serializer
+
+create_user_serializer = base_user_serializer | {
         'password': fields.String(required=True, 
                                   description='hash value of user password')                 
 }
@@ -13,16 +20,9 @@ create_user_serializer = {
 
 
 login_serializer = {
-    'email':fields.String(required=True, description='user email address'),
-    'password': fields.String(required=True, 
-                                  description='user input of password')
+        'email':fields.String(required=True, description='user email address'),
+        'password': fields.String(required=True, 
+                                        description='user input of password')
 }
 
-
-get_user_serializer = {
-        'id': fields.Integer(),
-        'first_name': fields.String(required=True, description='first name of user'),
-        'last_name': fields.String(required=True, description='last name of user'),
-        'username': fields.String(required=True, description='public display name of user')       
-}
 

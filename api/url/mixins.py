193184d529ace18dc818466a5a92fa6aebfc14b9
Priscilla Baah <pyfbaah@gmail.com>
2023-06-22 15:15:17 +0000

    check if code exists

--- api/url/mixins.py
+++ api/url/mixins.py
@@ -1,8 +1,14 @@
 import string
 import random
 
+from .models import Url
 
 class UrlHelpersMixin(object):
+
+    def url_code_exists(self, url_code):
+        url_obj = Url.query.filter_by(url_code=url_code).first()
+        
+        return url_obj != None
 
     def generate_short_text(self, request, n):
 
@@ -10,6 +16,10 @@ class UrlHelpersMixin(object):
 
         if not short_text:
             short_text = ''.join(random.choices(string.ascii_letters, k=n))
+        
+        while self.url_code_exists(short_text):
+            short_text = ''.join(random.choices(string.ascii_letters, k=n))
+        
         return short_text
     
     def get_url_domain(self, request):
@@ -17,7 +27,11 @@ class UrlHelpersMixin(object):
         if not domain:
             domain = request.url_root
 
+        if domain[-1] != '/':
+            domain += '/'
         return domain
+    
+    
     
       
         

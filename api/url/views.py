b4f1efd02240ba1ddb0afc4bc828c8d39b664714
Priscilla Baah <pyfbaah@gmail.com>
2023-06-22 15:17:09 +0000

    update url crud logic

--- api/url/views.py
+++ api/url/views.py
@@ -32,9 +32,17 @@ class ShortenUrl(Resource, UrlHelpersMixin):
 
         user_id = get_jwt_identity()
 
-        short_url = self.get_url_domain(request) + '/' + self.generate_short_text(request, 5)
+        url_obj = Url.query.filter_by(
+            target_url=data['target_url'],
+            user_id=user_id).first()
+        if url_obj:
+            return marshal(url_obj, shorten_url_response_model)
+
+        url_code = self.generate_short_text(request, 5)
+        short_url = self.get_url_domain(request)  + 'r/' + url_code
 
         new_url = Url(
+            url_code = url_code,
             target_url = data["target_url"],
             short_url = short_url,
             user_id = user_id,
@@ -49,26 +57,42 @@ class ShortenUrl(Resource, UrlHelpersMixin):
 
 @url_namespace.route('/urls')
 class GetUrls(Resource):
+    @jwt_required()
     def get(self):
         """
         Get all urls
         """
-        urls = Url.query.all()
+        user_id = get_jwt_identity()
+        urls = Url.query.filter_by(user_id=user_id).all()
+
+        response_data = []
+        for url in urls:
+
+            url_response = marshal(url, shorten_url_response_model)
+            response_data.append(url_response)
+
+        return response_data
+        
 
 
-@url_namespace.route('/<short_url>')
+@url_namespace.route('/r/<url_code>')
 class RedirectToTarget(Resource):
-    def get(self, short_url):
+    def get(self, url_code):
         """
         Redirect a given url to the destination url
         """
 
-        url_obj = Url.query.filter_by(short_url=short_url).first()
-        if url_obj:
-            target_url = url_obj.target_url
-            return {'redirect_url':target_url}
-        else:
-            return {'message': 'target url does not exist'}
+        url_obj = Url.query.filter_by(url_code=url_code).first().target_url
+
+        return redirect(url_obj)
+        # print("*********" + test_url+"***********************")
+        # url_obj = Url.query.filter_by(short_url=test_url).first()
+        # print(url_obj)
+        # if url_obj:
+        #     target_url = url_obj.target_url
+        #     return {'redirect_url':target_url}
+        # else:
+        #     return {'message': 'target url does not exist'}
 
 
        

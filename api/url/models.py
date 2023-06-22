ada8c54c0ce3f8f36b24bd1880fb3a7492e4230e
Priscilla Baah <pyfbaah@gmail.com>
2023-06-22 15:16:47 +0000

    use code as url model pk

--- api/url/models.py
+++ api/url/models.py
@@ -4,8 +4,8 @@ from api.utils import db
 from api.utils.mixins import ModelCreationMixin
 
 class Url(db.Model, ModelCreationMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    short_url = db.Column(db.String(10), nullable=False)
+    url_code = db.Column(db.String(5), primary_key=True)
+    short_url = db.Column(db.String(30), nullable=False)
     target_url = db.Column(db.String(150), nullable=False)
     clicks = db.Column(db.Integer)
     is_active = db.Column(db.Boolean(), default=True)

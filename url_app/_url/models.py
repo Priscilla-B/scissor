f102d9c8bdb8079526eecd901decdc4526d4571e
Priscilla Baah <pyfbaah@gmail.com>
2023-07-10 18:57:40 +0000

    update string maxlength limits

--- url_app/_url/models.py
+++ url_app/_url/models.py
@@ -4,9 +4,9 @@ from url_app.utils import db
 from url_app.utils.utils import ModelCreationMixin
 
 class Url(db.Model, ModelCreationMixin):
-    url_code = db.Column(db.String(5), primary_key=True)
-    short_url = db.Column(db.String(30), nullable=False)
-    target_url = db.Column(db.String(150), nullable=False)
+    url_code = db.Column(db.String(7), primary_key=True)
+    short_url = db.Column(db.String(150), nullable=False)
+    target_url = db.Column(db.String(), nullable=False)
     is_active = db.Column(db.Boolean(), default=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     user = db.relationship('User', backref='user')
